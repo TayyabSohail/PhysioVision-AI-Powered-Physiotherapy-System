5a3481921baace961e87fe56794f820cbf70b9f6
Abdullah Masood <abdullahmasood450@gmail.com>
2025-01-31 16:03:13 +0500

    feed: complete api for singup

---
+++ BACKEND/app/main.py
@@ -0,0 +1,62 @@
+from pymongo import MongoClient
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI app
+app = FastAPI()
+
+uri = "mongodb+srv://abdullahmasood450:harry_potter123@cluster0.ys9yt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+# Middleware for CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace '*' with your frontend URL in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+client = None
+db = None
+collection_users = None
+
+
+@app.on_event("startup")
+def startup_db_client():
+    global client, db, collection_users
+    client = MongoClient(uri)
+    db = client["PhysioVision"]
+    collection_users =  db["Users"]   
+    print("Connected to the MongoDB database!")
+
+
+# Pydantic model
+class UserSignUp(BaseModel):
+    name: str
+    username: str
+    email: str
+    password: str
+
+# User sign-up route
+@app.post("/api/signup")
+async def sign_up(user: UserSignUp):
+    try:
+        # Check if username or email already exists
+        existing_user = collection_users.find_one({"username": user.username})
+        existing_email = collection_users.find_one({"email": user.email})
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already exists. Try another one.")
+        
+        if existing_email:
+            raise HTTPException(status_code=400, detail="Email is already used. Try another one.")
+
+        # Convert Pydantic model to dictionary
+        user_data = user.dict()
+        result = collection_users.insert_one(user_data)  # Insert the document
+        return {"message": "User registered successfully!"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error: {str(e)}")
+
